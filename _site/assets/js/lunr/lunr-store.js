2a3fc8b702a1cdb917e32acb8d6eadc118d65152
chris-cornell <cornellc1645@gmail.com>
2018-02-02 22:56:36 -0500

    change index.html to properly use pagination!

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "First Post Placeholder",
+        "title": "A post!",
         "excerpt":"A test post to stay here until I figure out what I want to post about first.","categories": ["blog"],
         "tags": [],
-        "url": "http://localhost:4000/blog/2018/02/01/welcome-to-jekyll.html",
+        "url": "http://localhost:4000/blog/welcome-to-jekyll/",
         "teaser":null}]
