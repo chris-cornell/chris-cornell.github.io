61004f3f91fa1fd4d59c982597ff08632118e0ba
chris-cornell <cornellc1645@gmail.com>
2018-02-02 22:38:26 -0500

    install 'minimal-mistakes' theme and hope jekyll remote themes work

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "First Post Placeholder",
+        "excerpt":"A test post to stay here until I figure out what I want to post about first.","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2018/02/01/welcome-to-jekyll.html",
+        "teaser":null}]
