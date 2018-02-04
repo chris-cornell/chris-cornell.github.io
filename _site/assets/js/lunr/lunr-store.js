8413f0a5056431e543bb76f47c05fc1af96faa64
chris-cornell <cornellc1645@gmail.com>
2018-02-04 02:33:54 -0500

    add 'projects' page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Blog Beginnings",
+        "title": "Jekyll Installation and Google-Fu",
         "excerpt":"A couple months back, halfway into my first semester at Temple, I found myself in doubt. The first Computer Science class I registered for was CIS 1166 - “Mathematical Concepts in Computing I” - a Discrete Math course. Every section of CIS 1001, the intended introductory CS course, was full...","categories": ["jekyll"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/welcome-to-jekyll/",
+        "url": "http://localhost:4000/jekyll/Jekyll-Installation/",
         "teaser":null}]
---
+++ projects.md
@@ -0,0 +1,8 @@
+---
+layout: single
+title: Projects
+permalink: /projects/
+---
+Soon to be populated with projects!
+
+![Road Work Ahead!]({{ "/assets/roadwork.png" | absolute_url }})
